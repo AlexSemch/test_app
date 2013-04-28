ca831e8db160e072b5841378b6a268ce02119f98
AlexSemch <soi.semchenko@gmail.com>
2013-04-28 05:28:18 +0300

    -------------<<<<<<>>>>>>----------r--

--- db/schema.rb
+++ db/schema.rb
@@ -11,31 +11,4 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408082529) do
 
-  create_table "peoples", :force => true do |t|
-    t.string   "pip"
-    t.date     "birth_date"
-    t.string   "classchol"
-    t.boolean  "is_teacher", :default => false
-    t.integer  "user_id"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
-  end
-
-  add_index "people", ["user_id", "pip", "birth_date"], :name => "index_people_on_user_id_and_pip_and_birth_date"
-
-  create_table "users", :force => true do |t|
-    t.string   "nik"
-    t.string   "email"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
-    t.string   "password_digest"
-    t.string   "remember_token"
-    t.boolean  "admin",           :default => false
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
-
-end
